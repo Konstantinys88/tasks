838c868fa04ac669dcacdca6a15b2d0102fb5e59
Maschkov Konstantin <maschkovc@yandex.ru>
2023-07-12 11:25:43 +0300

    задачи из курса юдеми

---
+++ 09-15-lesson/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// интерполяция
+
+const category = "toys";
+console.log(`https://someurl.com/${category}/5`);
+
+//операторы
+
+//унарный плюс
+console.log(4 + +"4");
+
+//Инкремент и декремент
+let incr = 10,
+decr = 10;
+
+
+console.log(incr++);
+console.log(++incr);
+console.log(decr--);
+console.log(--decr);
+
+//остаток от деления
+console.log(5%2);
+
+
+
+
+
+
