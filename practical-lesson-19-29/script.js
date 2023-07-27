b60271fcf2ef3bce2b689b2c532af8207cc7e9f0
Maschkov Konstantin <maschkovc@yandex.ru>
2023-07-27 14:45:16 +0300

    37. Практика , ч4. Используем объекты

--- practical-lesson-19-29/script.js
+++ practical-lesson-19-29/script.js
@@ -16,68 +16,125 @@ const personalMovieDB = {
     movies: {},
     actors: {},
     genres: [],
-    privat: true,
-    start: function() {
+    privat: false,
+    start: function () {
         personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
         while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
             personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-        } 
+        }
+    },
+    lastMovieFunction: (dataBase) => {
+        for (let i = 0; i < 2; i++) {
+            const movie = prompt('Один из последних просмотренных фильмов?', '').trim();
+            const value = +prompt('На сколько оцените его?', '');
+            if (movie != null && value != null && movie != "" && value != "" && movie.length < 50) {
+                dataBase.movies[movie] = value;
+            } else {
+                console.log("Ошибка введите данные снова");
+                i--;
+            }
+        }
+
+        if (dataBase.count < 10) {
+            alert("Просмотрено довольно мало фильмов");
+        } else if (dataBase.count >= 10 && dataBase.count <= 30) {
+            alert("вы класический зритель");
+        } else if (dataBase.count > 30) {
+            alert("Вы киноман");
+        } else {
+            alert("Произошла ошибка");
+        }
+        return dataBase;
+    },
+    toggleVisibleMyDB: function (dataBase) {
+        if (dataBase.privat === true) {
+            dataBase.privat = false;
+        } else {
+            dataBase.privat = true;
+        }
+    },
+    writeYourGenre: function (dataBase) {
+        for (let i = 0; i < 3; i++) {
+            const genre = prompt(`Ваш любимый жанр под номером ${i + 1}`, '').trim();
+            if (genre != null && genre != '') {
+                dataBase.genres.push(genre);
+            } else {
+                console.log("Ошибка введите данные снова");
+                i--;
+            }
+        }
+        dataBase.genres.forEach((item, index) => {
+            console.log(`Любимый жанр №${index + 1} ${item}`);
+        })
+    },
+
+    showMyDB: function (dataBase) {
+        if (dataBase.privat === false) {
+            console.log("Данные закрыты");
+        } else {
+            console.log(personalMovieDB);
+        }
     },
 }
 
 personalMovieDB.start();
+personalMovieDB.lastMovieFunction(personalMovieDB);
+personalMovieDB.writeYourGenre(personalMovieDB);
 
-const lastMovieFunction = (dataBase) => {
-    for (let i = 0; i < 2; i++) {
-        const movie = prompt('Один из последних просмотренных фильмов?', '').trim();
-        const value = +prompt('На сколько оцените его?', '');
-        if (movie != null && value != null && movie != "" && value != "" && movie.length < 50) {
-            dataBase.movies[movie] = value;
-        } else {
-            console.log("Ошибка введите данные снова");
-            i--;
-        }
-    }
+personalMovieDB.toggleVisibleMyDB(personalMovieDB);
+personalMovieDB.showMyDB(personalMovieDB);
 
-    if (dataBase.count < 10) {
-        alert("Просмотрено довольно мало фильмов");
-    } else if (dataBase.count >= 10 && dataBase.count <= 30) {
-        alert("вы класический зритель");
-    } else if (dataBase.count > 30) {
-        alert("Вы киноман");
-    } else {
-        alert("Произошла ошибка");
-    }
+// const lastMovieFunction = (dataBase) => {
+//     for (let i = 0; i < 2; i++) {
+//         const movie = prompt('Один из последних просмотренных фильмов?', '').trim();
+//         const value = +prompt('На сколько оцените его?', '');
+//         if (movie != null && value != null && movie != "" && value != "" && movie.length < 50) {
+//             dataBase.movies[movie] = value;
+//         } else {
+//             console.log("Ошибка введите данные снова");
+//             i--;
+//         }
+//     }
 
-    return dataBase;
-}
+//     if (dataBase.count < 10) {
+//         alert("Просмотрено довольно мало фильмов");
+//     } else if (dataBase.count >= 10 && dataBase.count <= 30) {
+//         alert("вы класический зритель");
+//     } else if (dataBase.count > 30) {
+//         alert("Вы киноман");
+//     } else {
+//         alert("Произошла ошибка");
+//     }
 
-lastMovieFunction(personalMovieDB);
+//     return dataBase;
+// }
+
+// lastMovieFunction(personalMovieDB);
 
 
-function writeYourGenre(dataBase) {
-    for (let i = 0; i < 3; i++) {
-        const genre = prompt(`Ваш любимый жанр под номером ${i + 1}`, '');
-        if (genre != null && genre != '') {
-            dataBase.genres.push(genre);
-        } else {
-            console.log("Ошибка введите данные снова");
-            i--;
-        }     
-    }
-}
+// function writeYourGenre(dataBase) {
+//     for (let i = 0; i < 3; i++) {
+//         const genre = prompt(`Ваш любимый жанр под номером ${i + 1}`, '');
+//         if (genre != null && genre != '') {
+//             dataBase.genres.push(genre);
+//         } else {
+//             console.log("Ошибка введите данные снова");
+//             i--;
+//         }
+//     }
+// }
 
-writeYourGenre(personalMovieDB);
+// writeYourGenre(personalMovieDB);
 
-function showMyDB(dataBase) {
-    if (dataBase.privat == false) {
-        console.log("Данные закрыты");
-    } else {
-        console.log(personalMovieDB);
-    }
-}
+// function showMyDB(dataBase) {
+//     if (dataBase.privat == false) {
+//         console.log("Данные закрыты");
+//     } else {
+//         console.log(personalMovieDB);
+//     }
+// }
 
-showMyDB(personalMovieDB);
+// showMyDB(personalMovieDB);
 
 // console.log(personalMovieDB);
 
