b3b5d6f95b109dd8f2c8a3f31fa26b0834705471
Maschkov Konstantin <maschkovc@yandex.ru>
2023-08-03 12:14:09 +0300

    Упражнение по написанию кода 15

---
+++ 40-41-lesson/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+
+// function createCounter() {
+//     let counter = 0;
+
+//     const myFunction = function() {
+//         counter = counter + 1;
+//         return counter;
+//     }
+
+//     return myFunction;
+// }
+
+// const incr = createCounter();
+
+// console.log(incr);
+
+// const c1 = incr();
+// console.log(c1);
+// const c2 = incr();
+// console.log(c2);
+// const c3 = incr();
+// console.log(c3);
+
+// •	Какое будет выведено значение: 
+let x = 5; console.log( x++ );  //5
+
+// •	Чему равно такое выражение: [ ] + false - null + true ? 
+console.log(([ ] + false - null + true)) //NaN
+
+// •	Что выведет этот код: let y = 1; let x = y = 2; alert(x); ? 2
+
+// •	Чему равна сумма [ ] + 1 + 2? 12
+
+// •	Что выведет этот код: alert( "1"[0] )?
+console.log('1'[0]);
+
+// •	Чему равно 2 && 1 && null && 0 && undefined ? null
+
+// •	Есть ли разница между выражениями? !!( a && b ) и (a && b)?
+console.log(!!( 1 && 2 ))
+console.log(( 1 && 2 ))
+
+// •	Что выведет этот код: alert( null || 2 && 3 || 4 ); ? 3
+
+	let a = [1, 2, 3]; let b = [1, 2, 3]; //Правда ли что a == b ?
+    console.log(a === b)
+
+// •	Что выведет этот код: alert( +"Infinity" ); ?
+console.log(+"Infinity")
+
+// •	Верно ли сравнение: "Ёжик" > "яблоко"?
+console.log("Ёжик" > "яблоко")
+
+// •	Чему равно 0 || "" || 2 || undefined || true || falsе ?
+
+
+
