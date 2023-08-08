24768fe7b253abbfb614b386e6263be7fa8e1f47
Maschkov Konstantin <maschkovc@yandex.ru>
2023-08-08 19:10:16 +0300

    date

--- 64-lesson/script.js
+++ 64-lesson/script.js
@@ -1,24 +1,11 @@
 
-const btn = document.querySelector('.btn');
-let timerId;
-let iterator = 0;
-
-function animation() {
-    const elem = document.querySelector('.box');
-    let pos = 0;
-
-    const id = setInterval(frame, 10);
-    function frame() {
-        if(pos == 300) {
-            clearInterval();
-        } else {
-            pos++;
-            elem.style.top = `${pos}px`;
-            elem.style.left = `${pos}px`;
-        }
-    }
-}
-
-btn.addEventListener('click', animation);
 
 
+const now = new Date();
+// console.log(now.getHours());
+console.log(now.setHours(100));
+console.log(now);
+
+
+
+console.log(10 ** 2);
