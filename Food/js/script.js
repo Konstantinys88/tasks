e74a17a8d74dd4bbebf9b731aa8af5b40cacf1ea
Maschkov Konstantin <maschkovc@yandex.ru>
2023-08-09 13:23:15 +0300

    modal scroll

--- Food/js/script.js
+++ Food/js/script.js
@@ -114,23 +114,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const btnsModal = document.querySelectorAll('[data-modal]');
     const modal = document.querySelector('.modal');
     const modalClose = document.querySelector('.modal__close');
-    const modalDialog = document.querySelector('.modal__dialog');
 
-    function schowModal() {
-        btnsModal.forEach(item => {
-            item.addEventListener('click', () => {
-                modal.classList.add('show');
-                document.body.style.overflow = 'hidden';
-            })
-        })
+
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
     }
-
-    schowModal();
 
     function closeModal() {
         modal.classList.remove('show');
+        modal.classList.add('hide');
         document.body.style.overflow = '';
     }
+
+    btnsModal.forEach(item => {
+        item.addEventListener('click', () => {
+            openModal();
+        })
+    })
 
     modalClose.addEventListener('click', closeModal);
 
@@ -140,11 +143,24 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    document.addEventListener('keydown' , (e) => {
-        if(e.code === 'Escape' && modal.classList.contains('show')) {
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
             closeModal();
         }
     });
+
+    const modalTimerId = setTimeout(openModal, 30000);
+
+    function showModalByScroll() {
+        if(window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+
+
 
 
 });
