37c68926be51ff21eac69b1fc3e78a50008a361a
Maschkov Konstantin <maschkovc@yandex.ru>
2023-08-09 13:47:58 +0300

    NutationObserver

---
+++ 74-lesson/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const box = document.querySelector('.box');
+
+let observer = new MutationObserver(mutationRecord => {
+    console.log(mutationRecord)
+});
+
+observer.observe(box, {
+    childList: true
+})
